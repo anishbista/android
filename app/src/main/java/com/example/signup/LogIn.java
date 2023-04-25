2564019765f670db6b486f956865d8533d076782
@anishbista92 <anishbista9235@gmail.com>
2023-04-25 13:10:30 +0545

    Sign In and Sign Up Tuesday

--- app/src/main/java/com/example/signup/LogIn.java
+++ app/src/main/java/com/example/signup/LogIn.java
@@ -1,9 +1,15 @@
 package com.example.signup;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.Toolbar;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -14,13 +20,24 @@ import android.widget.Toast;
 public class LogIn extends AppCompatActivity implements View.OnClickListener {
     private EditText edtEmail, edtPass;
     private AppCompatButton btnSignIn;
+    private Toolbar toolbar;
     private TextView txtBCA, txtWelcome, txtForgot, txtAlready, txtSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+        initToolbar();
         findView();
+
+    }
+
+    private void initToolbar() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setTitle("Login Page");
     }
 
     private void findView() {
@@ -45,22 +62,61 @@ public class LogIn extends AppCompatActivity implements View.OnClickListener {
     @Override
     public void onClick(View view) {
         if (view.getId() == R.id.btnSignIn) {
+            if(edtEmail.getText().toString().isEmpty() || edtPass.getText().toString().isEmpty()){
+
+                if (edtEmail.getText().toString().isEmpty() && !edtPass.getText().toString().isEmpty() ){
+                    Toast.makeText(this, "Please enter Email", Toast.LENGTH_LONG).show();
+                }
+                else if (edtPass.getText().toString().isEmpty() && !edtEmail.getText().toString().isEmpty() ){
+                    Toast.makeText(this, "Please enter Password", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, "Please enter Email and Password", Toast.LENGTH_LONG).show();
+
+                }
+            }
+            else if (!edtEmail.getText().toString().isEmpty() && !edtPass.getText().toString().isEmpty()) {
+                Intent intent = new Intent(LogIn.this, Dashboard.class);
+                Toast.makeText(this, "You have logged in", Toast.LENGTH_LONG).show();
+//            intent.putExtra("Email",edtEmail.getText().toString().trim());
+//                intent.putExtra("Email", "Email Address");
+            startActivity(intent);
+//            startActivityForResult(intent,100);
+
+
+            }
+
+
+
         } else if (view.getId() == R.id.txtSignUp) {
             Intent intent = new Intent(LogIn.this, SignupActivity.class);
 //            intent.putExtra("Email",edtEmail.getText().toString().trim());
-            intent.putExtra("Email","Email Address");
+            intent.putExtra("Email", "Email Address");
 //            startActivity(intent);
-            startActivityForResult(intent,100);
-
+//            startActivityForResult(intent,100);
+            signupActivityLauncher.launch(intent);
 
 
         }
     }
 
+    ActivityResultLauncher<Intent> signupActivityLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        Toast.makeText(LogIn.this, data.getStringExtra("Result"), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
+    );
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if(requestCode==100&& resultCode == RESULT_OK){
-            Toast.makeText(this,data.getStringExtra(""+""),Toast.LENGTH_SHORT).show();
+        if (requestCode == 100 && resultCode == RESULT_OK) {
+            Toast.makeText(this, data.getStringExtra("" + ""), Toast.LENGTH_SHORT).show();
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
--- app/src/main/java/com/example/signup/SignupActivity.java
+++ app/src/main/java/com/example/signup/SignupActivity.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,8 +16,10 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
 
 
 private TextView txtAccountSignIn;
-private AppCompatButton btnSignUp;
-String email;
+private EditText edtName,edtEmail,edtPass,edtConfPass;
+private CheckBox boxTerms;
+ AppCompatButton btnSignUp;
+String email = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,21 +28,38 @@ String email;
         email=getIntent().getStringExtra("Email");
         Toast.makeText(this, "email", Toast.LENGTH_SHORT).show();
         Log.d("Email Address",email);
-//        findview();
+        findview();
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent();
-                intent.putExtra("Result","Result Return");
-                setResult(RESULT_OK,intent);
-                finish();
+                if (view.getId() == R.id.btnSignUp) {
+                    if (edtName.getText().toString().isEmpty() || edtEmail.getText().toString().isEmpty() || edtPass.getText().toString().isEmpty() || edtConfPass.getText().toString().isEmpty()) {
+
+                        if (edtName.getText().toString().isEmpty() && !edtEmail.getText().toString().isEmpty() && !edtPass.getText().toString().isEmpty() && !edtConfPass.getText().toString().isEmpty()) {
+                            Toast.makeText(this, "Please enter Name", Toast.LENGTH_SHORT).show();
+                        } else if (edtName.getText().toString().isEmpty() && !edtEmail.getText().toString().isEmpty() && !edtPass.getText().toString().isEmpty() && !edtConfPass.getText().toString().isEmpty()) {
+                            Toast.makeText(this, "Please enter Name", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(this, "Please enter Email and Password", Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+                    Intent intent = new Intent();
+                    intent.putExtra("Result", "Result Return");
+                    setResult(RESULT_OK, intent);
+                    finish();
 
 
+                }
             }
         });
     }
     private void findview(){
-
+        edtName = findViewById(R.id.edtName);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPass = findViewById(R.id.edtPass);
+        edtConfPass = findViewById(R.id.edtConfPass);
+        boxTerms = findViewById(R.id.boxTerms);
         btnSignUp  = findViewById(R.id.btnSignUp);
         txtAccountSignIn = findViewById(R.id.txtAccountSignIn);
         btnSignUp.setOnClickListener(this);
@@ -58,3 +79,4 @@ String email;
     }
 
 }
+
